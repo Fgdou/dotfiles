9e7cad773999d1f167fcb99d06630280ad5c5226
Fgdou <fabigoardou@gmail.com>
2024-02-26 16:40:50 +0100

    update backgriound

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -32,14 +32,14 @@ in {
     desktopManager.cinnamon = {
       enable = true;
       extraGSettingsOverrides = ''
-      [org/cinnamon]
+[org/cinnamon]
 desktop-effects-workspace=false
 enabled-applets=['panel1:center:0:menu@cinnamon.org:0', 'panel1:center:1:grouped-window-list@cinnamon.org:2', 'panel1:right:3:systray@cinnamon.org:3', 'panel1:right:4:xapp-status@cinnamon.org:4', 'panel1:right:5:notifications@cinnamon.org:5', 'panel1:right:6:printers@cinnamon.org:6', 'panel1:right:7:removable-drives@cinnamon.org:7', 'panel1:right:8:keyboard@cinnamon.org:8', 'panel1:right:10:network@cinnamon.org:10', 'panel1:right:11:sound@cinnamon.org:11', 'panel1:right:12:power@cinnamon.org:12', 'panel1:right:13:calendar@cinnamon.org:13']
 next-applet-id=18
 
 [org/cinnamon/desktop/background]
 picture-options='zoom'
-picture-uri='file:///home/fgdou/dotfiles/assets/Wolf galaxy1470_square.jpg'
+picture-uri='file:///home/fgdou/dotfiles/assets/Wolf.jpg'
 
 [org/cinnamon/desktop/background/slideshow]
 image-source='directory:///home/fgdou/dotfiles/assets'
@@ -61,6 +61,9 @@ color-scheme='prefer-dark'
     };
     layout = "us";
     xkbVariant = "intl";
+    displayManager.lightdm.greeters.slick.extraConfig = ''
+    background=/home/fgdou/dotfiles/assets/Wolf.jpg
+    '';
   };
   services.cinnamon.apps.enable = true;
 
