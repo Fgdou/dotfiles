19192a0e5c08d355c9edff3e3989c49fe2b6c9ed
Fgdou <fabigoardou@gmail.com>
2024-02-29 19:38:23 +0100

    capslock

--- nixos/users.nix
+++ nixos/users.nix
@@ -15,28 +15,33 @@
 
     dconf = {
       enable = true;
-      settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
-      settings."org/cinnamon/desktop/background".picture-uri = "file:///home/fgdou/dotfiles/assets/Wolf.jpg";
-      settings."org/cinnamon" = {
-        desktop-effects-workspace = "false";
-        enabled-applets = [
-          "panel1:center:0:menu@cinnamon.org:0"
-          "panel1:center:1:grouped-window-list@cinnamon.org:2"
-          "panel1:right:3:systray@cinnamon.org:3"
-          "panel1:right:4:xapp-status@cinnamon.org:4"
-          "panel1:right:5:notifications@cinnamon.org:5"
-          "panel1:right:6:printers@cinnamon.org:6"
-          "panel1:right:7:removable-drives@cinnamon.org:7"
-          "panel1:right:8:keyboard@cinnamon.org:8"
-          "panel1:right:10:network@cinnamon.org:10"
-          "panel1:right:11:sound@cinnamon.org:11"
-          "panel1:right:12:power@cinnamon.org:12"
-          "panel1:right:13:calendar@cinnamon.org:13"
+      settings = {
+        "org/gnome/desktop/interface".color-scheme = "prefer-dark";
+        "org/cinnamon/desktop/background".picture-uri = "file:///home/fgdou/dotfiles/assets/Wolf.jpg";
+        "org/cinnamon" = {
+          desktop-effects-workspace = "false";
+          enabled-applets = [
+            "panel1:center:0:menu@cinnamon.org:0"
+            "panel1:center:1:grouped-window-list@cinnamon.org:2"
+            "panel1:right:3:systray@cinnamon.org:3"
+            "panel1:right:4:xapp-status@cinnamon.org:4"
+            "panel1:right:5:notifications@cinnamon.org:5"
+            "panel1:right:6:printers@cinnamon.org:6"
+            "panel1:right:7:removable-drives@cinnamon.org:7"
+            "panel1:right:8:keyboard@cinnamon.org:8"
+            "panel1:right:10:network@cinnamon.org:10"
+            "panel1:right:11:sound@cinnamon.org:11"
+            "panel1:right:12:power@cinnamon.org:12"
+            "panel1:right:13:calendar@cinnamon.org:13"
+          ];
+        };
+        "org/nemo/preferences".show-hidden-files = "true";
+        "org/x/apps/portal".color-scheme = "prefer-dark";
+        "org/cinnamon/desktop/background".picture-options = "zoom";
+        "org/gnome/libgnomekbd/keyboard".options= [
+          "caps\tcaps:none"
         ];
       };
-      settings."org/nemo/preferences".show-hidden-files = "true";
-      settings."org/x/apps/portal".color-scheme = "prefer-dark";
-      settings."org/cinnamon/desktop/background".picture-options = "zoom";
     };
   };
 }
